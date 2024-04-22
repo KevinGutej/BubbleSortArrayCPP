c4b5266478494a90790760a9f1468d2a8c00e41e
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2024-04-22 22:44:47 +0200

    Added functions heapify and swap

--- heapSort.cpp
+++ heapSort.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
 #include <vector>
 
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void heapify(vector<int> &arr, int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < n && arr[l] > arr[largest])
+    {
+        largest = l;
+    }
+    if (r < n && arr[r] > arr[largest])
+    {
+        largest = r;
+    }
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
 void heapSort(vector<int> &arr)
 {
     int n = arr.size();
     for (int i = n / 2 - 1; i >= 0; --i)
     {
-        heapify(); // create a function called heapify
+        heapify(arr, n, i);
     }
     for (int i = n - 1; i > 0; --i)
     {
-        swap(); // create function swap
-        heapify();
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
     }
 }
 
